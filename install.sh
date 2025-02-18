98ec1bfb92521aad6e892e4e391695f1b5a14d2e
xziya <zxwei@mail.ecust.edu.cn>
2025-02-18 13:10:50 +0800

    lazyvim: update the directory hierachy

--- install.sh
+++ install.sh
@@ -6,15 +6,15 @@ white='\e[0;37m'
 dotfiles_repo_dir=$(pwd)
 backup_dir="$HOME/.dotfiles.orig"
 dotfiles_home_dir=(.zsh .aliases .bash_profile .bashrc .dircolors .editorconfig
-                   .functions .gemrc .ripgreprc .wgetrc .Xresources .zshrc .tmux.conf
-                   .condarc .config/nvim)
-dotfiles_xdg_config_dir=(.alacritty .dunst .htop .i3 .i3blocks .picom .rofi .tmux)
+        .functions .gemrc .ripgreprc .wgetrc .Xresources .zshrc .tmux.conf
+        .condarc)
+dotfiles_xdg_config_dir=(.alacritty .dunst .htop .i3 .i3blocks .picom .rofi .tmux nvim)
 
 # Print usage message.
 usage() {
-    local program_name
-    program_name=${0##*/}
-    cat <<EOF
+        local program_name
+        program_name=${0##*/}
+        cat <<EOF
 Usage: $program_name [-option]
 
 Options:
@@ -25,123 +25,118 @@ EOF
 }
 
 _init_ohmyzsh() {
-    if ! [ -d "$HOME/.oh-my-zsh" ]; then
-        sh .oh-my-zsh/install.sh
-    fi
-    ZSH="${HOME}/.oh-my-zsh" ${dotfiles_repo_dir}/.oh-my-zsh/custom/install_ohmyzsh_custom.sh
+        if ! [ -d "$HOME/.oh-my-zsh" ]; then
+                sh .oh-my-zsh/install.sh
+        fi
+        ZSH="${HOME}/.oh-my-zsh" ${dotfiles_repo_dir}/.oh-my-zsh/custom/install_ohmyzsh_custom.sh
 }
 
 install_dotfiles() {
-    # Backup config.
-    if ! [ -f "$backup_dir/check-backup.txt" ]; then
-        mkdir -p "$backup_dir/.config"
-        cd "$backup_dir" || exit
-        touch check-backup.txt
+        # Backup config.
+        if ! [ -f "$backup_dir/check-backup.txt" ]; then
+                mkdir -p "$backup_dir/.config"
+                cd "$backup_dir" || exit
+                touch check-backup.txt
 
-        # Backup to ~/.dotfiles.orig
-        for dots_home in "${dotfiles_home_dir[@]}"
-        do
-            env cp -rf "$HOME/${dots_home}" "$backup_dir" &> /dev/null
-        done
+                # Backup to ~/.dotfiles.orig
+                for dots_home in "${dotfiles_home_dir[@]}"; do
+                        env cp -rf "$HOME/${dots_home}" "$backup_dir" &>/dev/null
+                done
 
-        # Backup some folder in ~/.config to ~/.dotfiles.orig/.config
-        for dots_xdg_conf in "${dotfiles_xdg_config_dir[@]//./}"
-        do
-            env cp -rf "$HOME/.config/${dots_xdg_conf}" "$backup_dir/.config" &> /dev/null
-        done
+                # Backup some folder in ~/.config to ~/.dotfiles.orig/.config
+                for dots_xdg_conf in "${dotfiles_xdg_config_dir[@]//./}"; do
+                        env cp -rf "$HOME/.config/${dots_xdg_conf}" "$backup_dir/.config" &>/dev/null
+                done
 
-        # Backup again with Git.
-        if [ -x "$(command -v git)" ]; then
-            git init &> /dev/null
-            git add -u &> /dev/null
-            git add . &> /dev/null
-            git commit -m "Backup original config on $(date '+%Y-%m-%d %H:%M')" &> /dev/null
+                # Backup again with Git.
+                if [ -x "$(command -v git)" ]; then
+                        git init &>/dev/null
+                        git add -u &>/dev/null
+                        git add . &>/dev/null
+                        git commit -m "Backup original config on $(date '+%Y-%m-%d %H:%M')" &>/dev/null
+                fi
+
+                # Output.
+                echo -e "${blue}Your config is backed up in ${backup_dir}\n" >&2
+                echo -e "${red}Please do not delete check-backup.txt in .dotfiles.orig folder.${white}" >&2
+                echo -e "It's used to backup and restore your old config.\n" >&2
         fi
 
-        # Output.
-        echo -e "${blue}Your config is backed up in ${backup_dir}\n" >&2
-        echo -e "${red}Please do not delete check-backup.txt in .dotfiles.orig folder.${white}" >&2
-        echo -e "It's used to backup and restore your old config.\n" >&2
-    fi
+        # init ohmyzsh
+        _init_ohmyzsh
 
-    # init ohmyzsh
-    _init_ohmyzsh
+        # Install config.
+        for dots_home in "${dotfiles_home_dir[@]}"; do
+                env rm -rf "$HOME/${dots_home}"
+                env ln -fs "$dotfiles_repo_dir/${dots_home}" "$HOME/"
+        done
 
-    # Install config.
-    for dots_home in "${dotfiles_home_dir[@]}"
-    do
-        env rm -rf "$HOME/${dots_home}"
-        env ln -fs "$dotfiles_repo_dir/${dots_home}" "$HOME/"
-    done
+        # Install config to ~/.config.
+        mkdir -p "$HOME/.config"
+        for dots_xdg_conf in "${dotfiles_xdg_config_dir[@]}"; do
+                env rm -rf "$HOME/.config/${dots_xdg_conf[*]//./}"
+                env ln -fs "$dotfiles_repo_dir/${dots_xdg_conf}" "$HOME/.config/${dots_xdg_conf[*]//./}"
+        done
 
-    # Install config to ~/.config.
-    mkdir -p "$HOME/.config"
-    for dots_xdg_conf in "${dotfiles_xdg_config_dir[@]}"
-    do
-        env rm -rf "$HOME/.config/${dots_xdg_conf[*]//./}"
-        env ln -fs "$dotfiles_repo_dir/${dots_xdg_conf}" "$HOME/.config/${dots_xdg_conf[*]//./}"
-    done
-
-    echo -e "${blue}New dotfiles is installed!\n${white}" >&2
-    echo "There may be some errors when Terminal is restarted." >&2
-    echo "Please read carefully the error messages and make sure all packages are installed. See more info in README.md." >&2
-    echo "Note that the author of this dotfiles uses dev branch in some packages." >&2
-    echo -e "If you want to restore your old config, you can use ${red}./install.sh -r${white} command." >&2
+        echo -e "${blue}New dotfiles is installed!\n${white}" >&2
+        echo "There may be some errors when Terminal is restarted." >&2
+        echo "Please read carefully the error messages and make sure all packages are installed. See more info in README.md." >&2
+        echo "Note that the author of this dotfiles uses dev branch in some packages." >&2
+        echo -e "If you want to restore your old config, you can use ${red}./install.sh -r${white} command." >&2
 }
 
 uninstall_dotfiles() {
-    if [ -f "$backup_dir/check-backup.txt" ]; then
-        for dots_home in "${dotfiles_home_dir[@]}"
-        do
-            env rm -rf "$HOME/${dots_home}"
-            env cp -rf "$backup_dir/${dots_home}" "$HOME/" &> /dev/null
-            env rm -rf "$backup_dir/${dots_home}"
-        done
+        if [ -f "$backup_dir/check-backup.txt" ]; then
+                for dots_home in "${dotfiles_home_dir[@]}"; do
+                        env rm -rf "$HOME/${dots_home}"
+                        env cp -rf "$backup_dir/${dots_home}" "$HOME/" &>/dev/null
+                        env rm -rf "$backup_dir/${dots_home}"
+                done
 
-        for dots_xdg_conf in "${dotfiles_xdg_config_dir[@]//./}"
-        do
-            env rm -rf "$HOME/.config/${dots_xdg_conf}"
-            env cp -rf "$backup_dir/.config/${dots_xdg_conf}" "$HOME/.config" &> /dev/null
-            env rm -rf "$backup_dir/.config/${dots_xdg_conf}"
-        done
+                for dots_xdg_conf in "${dotfiles_xdg_config_dir[@]//./}"; do
+                        env rm -rf "$HOME/.config/${dots_xdg_conf}"
+                        env cp -rf "$backup_dir/.config/${dots_xdg_conf}" "$HOME/.config" &>/dev/null
+                        env rm -rf "$backup_dir/.config/${dots_xdg_conf}"
+                done
 
-        # Save old config in backup directory with Git.
-        if [ -x "$(command -v git)" ]; then
-            cd "$backup_dir" || exit
-            git add -u &> /dev/null
-            git add . &> /dev/null
-            git commit -m "Restore original config on $(date '+%Y-%m-%d %H:%M')" &> /dev/null
+                # Save old config in backup directory with Git.
+                if [ -x "$(command -v git)" ]; then
+                        cd "$backup_dir" || exit
+                        git add -u &>/dev/null
+                        git add . &>/dev/null
+                        git commit -m "Restore original config on $(date '+%Y-%m-%d %H:%M')" &>/dev/null
+                fi
         fi
-    fi
 
-    if ! [ -f "$backup_dir/check-backup.txt" ]; then
-        echo -e "${red}You have not installed this dotfiles yet.${white}" >&2
-        exit 1
-    else
-        echo -e "${blue}Your old config has been restored!\n${white}" >&2
-        echo "Thanks for using my dotfiles." >&2
-        echo "Enjoy your next journey!" >&2
-    fi
+        if ! [ -f "$backup_dir/check-backup.txt" ]; then
+                echo -e "${red}You have not installed this dotfiles yet.${white}" >&2
+                exit 1
+        else
+                echo -e "${blue}Your old config has been restored!\n${white}" >&2
+                echo "Thanks for using my dotfiles." >&2
+                echo "Enjoy your next journey!" >&2
+        fi
 
-    env rm -rf "$backup_dir/check-backup.txt"
+        env rm -rf "$backup_dir/check-backup.txt"
 }
 
 main() {
-    case "$1" in
-        ''|-h|--help)
-            usage
-            exit 0
-            ;;
+        case "$1" in
+        '' | -h | --help)
+                usage
+                exit 0
+                ;;
         -i)
-            install_dotfiles
-            ;;
+                install_dotfiles
+                ;;
         -r)
-            uninstall_dotfiles
-            ;;
+                uninstall_dotfiles
+                ;;
         *)
-            echo "Command not found" >&2
-            exit 1
-    esac
+                echo "Command not found" >&2
+                exit 1
+                ;;
+        esac
 }
 
 main "$@"
